ed236a20cf43167367a67dde448b9dbf384a00cf
Nam V. Do <macbook@MacBook-Pro-cua-MacBook.local>
2019-01-29 14:24:10 +0700

    Tic Tac Toe Game on Ruby

---
+++ lib/move.rb
@@ -0,0 +1,22 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+input = "1", "2", "3", "4", "5", "6", "7", "8", "9"
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+board = [" "," "," "]
+def move(input, index, character = "X")
+  return input[index] = character
+end
+move(board, 0, "X")
+board
+input
